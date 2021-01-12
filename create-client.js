cfa64fd65ca8421ee9360259bf9ac63a328106c1
Ivan Ushmorov <ivan.ushmorov@zeiss.com>
2021-01-12 15:57:05 +0100

    added basic clients api

---
+++ create-client.js
@@ -0,0 +1,21 @@
+import * as uuid from "uuid";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    const data = JSON.parse(event.body);
+    const params = {
+      TableName: process.env.tableName,
+      Item: {
+        // The attributes of the item to be created
+        userId: "123", // The id of the author
+        swClientId: uuid.v1(), // A unique uuid
+        swClientName: data.name, // Parsed from request body
+        createdAt: Date.now(), // Current Unix timestamp
+      },
+    };
+
+    await dynamoDb.put(params);
+
+    return params.Item;
+  });
